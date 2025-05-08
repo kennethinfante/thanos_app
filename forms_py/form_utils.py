f1727955226e426975cdfb8b257729fc412bd0a6
Kenneth <infantekennethjhim@gmail.com>
2025-05-08 20:16:33 +0800

    Refactored the widget creation to form_utils

---
+++ forms_py/form_utils.py
@@ -0,0 +1,154 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+def create_checkbox(parent, text, font_size=12, bold=True, direction=QtCore.Qt.RightToLeft, object_name=None):
+    """Create a standardized label with consistent styling
+
+    Args:
+        parent: Parent widget
+        text: Label text
+        font_size: Font size (default: 12)
+        bold: Whether font should be bold (default: True)
+        direction: Button and text direction (default: QtCore.Qt.RightToLeft)
+        object_name: Object name for the label (optional)
+
+    Returns:
+        QCheckbox: The created checkbox
+    """
+
+    chkbox = QtWidgets.QCheckBox(parent)
+    chkbox.setText(text)
+    font = QtGui.QFont()
+    font.setPointSize(font_size)
+    chkbox.setFont(font)
+
+    if bold:
+        font.setBold(bold)
+    if direction:
+        chkbox.setLayoutDirection(direction)
+    if object_name:
+        chkbox.setObjectName(object_name)
+
+    return chkbox
+
+def create_label(parent, text, font_size=12, bold=True, object_name=None):
+    """Create a standardized label with consistent styling
+
+    Args:
+        parent: Parent widget
+        text: Label text
+        font_size: Font size (default: 12)
+        bold: Whether font should be bold (default: True)
+        object_name: Object name for the label (optional)
+
+    Returns:
+        QLabel: The created label
+    """
+    label = QtWidgets.QLabel(parent)
+    font = QtGui.QFont()
+    font.setPointSize(font_size)
+    font.setBold(bold)
+    label.setFont(font)
+    label.setText(text)
+    if object_name:
+        label.setObjectName(object_name)
+    return label
+
+def create_button(parent, text, min_size=(120, 40), font_size=12,
+                 bold=True, tooltip=None, icon_path=None, object_name=None):
+    """Create a standardized button with consistent styling
+
+    Args:
+        parent: Parent widget
+        text: Button text
+        min_size: Minimum size (default: (120, 40))
+        font_size: Font size (default: 12)
+        bold: Whether font should be bold (default: True)
+        tooltip: Tooltip text (optional)
+        icon_path: Path to icon (optional)
+        object_name: Object name for the button (optional)
+
+    Returns:
+        QPushButton: The created button
+    """
+    button = QtWidgets.QPushButton(parent)
+    button.setMinimumSize(QtCore.QSize(*min_size))
+    font = QtGui.QFont()
+    font.setPointSize(font_size)
+    font.setBold(bold)
+    button.setFont(font)
+    button.setText(text)
+
+    if tooltip:
+        button.setToolTip(tooltip)
+    if icon_path:
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(icon_path), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        button.setIcon(icon)
+    if object_name:
+        button.setObjectName(object_name)
+
+    return button
+
+def create_line_edit(parent, min_size=(200, 40), font_size=12,
+                    bold=False, placeholder=None, object_name=None):
+    """Create a standardized line edit with consistent styling
+
+    Args:
+        parent: Parent widget
+        min_size: Minimum size (default: (200, 40))
+        font_size: Font size (default: 12)
+        bold: Whether font should be bold (default: False)
+        placeholder: Placeholder text (optional)
+        object_name: Object name for the line edit (optional)
+
+    Returns:
+        QLineEdit: The created line edit
+    """
+    line_edit = QtWidgets.QLineEdit(parent)
+    line_edit.setMinimumSize(QtCore.QSize(*min_size))
+    font = QtGui.QFont()
+    font.setPointSize(font_size)
+    font.setBold(bold)
+    line_edit.setFont(font)
+
+    if placeholder:
+        line_edit.setPlaceholderText(placeholder)
+    if object_name:
+        line_edit.setObjectName(object_name)
+
+    return line_edit
+
+def create_date_edit(parent, enabled=True, min_size=(160, 40),
+                        font_size=12, bold=True, display_format="yyyy-MM-dd",
+                        default_date=None, object_name=None):
+    """Create a standardized date edit with consistent styling
+
+    Args:
+        parent: Parent widget
+        enabled: Whether the date edit is enabled (default: True)
+        min_size: Minimum size (default: (160, 40))
+        font_size: Font size (default: 12)
+        bold: Whether font should be bold (default: True)
+        display_format: Date display format (default: "yyyy-MM-dd")
+        default_date: Default date (QDate object, optional)
+        object_name: Object name for the date edit (optional)
+
+    Returns:
+        QDateEdit: The created date edit
+    """
+    date_edit = QtWidgets.QDateEdit(parent)
+    date_edit.setEnabled(enabled)
+    date_edit.setMinimumSize(QtCore.QSize(*min_size))
+    font = QtGui.QFont()
+    font.setPointSize(font_size)
+    font.setBold(bold)
+    date_edit.setFont(font)
+    date_edit.setCalendarPopup(True)
+    date_edit.setDisplayFormat(display_format)
+
+    if default_date:
+        date_edit.setDate(QtCore.QDate(*default_date))
+    if object_name:
+        date_edit.setObjectName(object_name)
+
+    return date_edit
--- forms_py/invoice_list_view.py
+++ forms_py/invoice_list_view.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from .form_styles import *
+from .form_utils import *
 
 class Ui_invoiceListView(object):
     def setupUi(self, invoiceListView):
@@ -34,46 +35,39 @@ class Ui_invoiceListView(object):
         self.search_hly.setObjectName("search_hly")
 
         # Date filter checkbox
-        self.search_date_chbox = QtWidgets.QCheckBox(self.header_section)
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        font.setBold(True)
-        self.search_date_chbox.setFont(font)
-        self.search_date_chbox.setText("Enable date")
-        self.search_date_chbox.setLayoutDirection(QtCore.Qt.RightToLeft)
-        self.search_date_chbox.setObjectName("search_date_chbox")
+        self.search_date_chbox = create_checkbox(self.header_section, "Enable date")
         self.search_hly.addWidget(self.search_date_chbox)
 
         # From date label
-        self.from_date_lbl = self.create_label(self.header_section, "From date:")
+        self.from_date_lbl = create_label(self.header_section, "From date:")
         self.search_hly.addWidget(self.from_date_lbl)
 
         # From date edit
-        self.from_date_dte = self.create_date_edit(self.header_section, False, default_date=(2022, 1, 1))
+        self.from_date_dte = create_date_edit(self.header_section, False, default_date=(2022, 1, 1))
         self.search_hly.addWidget(self.from_date_dte)
 
         # To date label
-        self.to_date_lbl = self.create_label(self.header_section, "To date:")
+        self.to_date_lbl = create_label(self.header_section, "To date:")
         self.search_hly.addWidget(self.to_date_lbl)
 
         # To date edit
-        self.to_date_dte = self.create_date_edit(self.header_section, False, default_date=(2024, 1, 1))
+        self.to_date_dte = create_date_edit(self.header_section, False, default_date=(2024, 1, 1))
         self.search_hly.addWidget(self.to_date_dte)
 
         # Customer label
-        self.customer_lbl = self.create_label(self.header_section, "Customer")
+        self.customer_lbl = create_label(self.header_section, "Customer")
         self.search_hly.addWidget(self.customer_lbl)
 
         # Customer line edit
-        self.customer_lne = self.create_line_edit(self.header_section, placeholder="Enter customer name")
+        self.customer_lne = create_line_edit(self.header_section, placeholder="Enter customer name")
         self.search_hly.addWidget(self.customer_lne)
 
         # Search button
-        self.search_btn = self.create_button(self.header_section, "Search")
+        self.search_btn = create_button(self.header_section, "Search")
         self.search_hly.addWidget(self.search_btn)
 
         # Clear button
-        self.clear_btn = self.create_button(self.header_section, "Clear")
+        self.clear_btn = create_button(self.header_section, "Clear")
         self.search_hly.addWidget(self.clear_btn)
 
         # Add search criteria layout to search section
@@ -85,11 +79,11 @@ class Ui_invoiceListView(object):
         self.btn_hly.setObjectName("btn_hly")
 
         # Add new invoice button
-        self.add_new_invoice_btn = self.create_button(self.header_section, "Add New Invoice", min_size=(180, 40))
+        self.add_new_invoice_btn = create_button(self.header_section, "Add New Invoice", min_size=(180, 40))
         self.btn_hly.addWidget(self.add_new_invoice_btn)
 
         # Refresh list button
-        self.refresh_list_btn = self.create_button(self.header_section, "Refresh List", min_size=(150, 40))
+        self.refresh_list_btn = create_button(self.header_section, "Refresh List", min_size=(150, 40))
         self.btn_hly.addWidget(self.refresh_list_btn)
 
         # Add spacer to push buttons to the left
@@ -159,125 +153,4 @@ class Ui_invoiceListView(object):
 
         apply_line_edit_style(self.customer_lne)
 
-    def create_label(self, parent, text, font_size=12, bold=True, object_name=None):
-        """Create a standardized label with consistent styling
 
-        Args:
-            parent: Parent widget
-            text: Label text
-            font_size: Font size (default: 12)
-            bold: Whether font should be bold (default: True)
-            object_name: Object name for the label (optional)
-
-        Returns:
-            QLabel: The created label
-        """
-        label = QtWidgets.QLabel(parent)
-        font = QtGui.QFont()
-        font.setPointSize(font_size)
-        font.setBold(bold)
-        label.setFont(font)
-        label.setText(text)
-        if object_name:
-            label.setObjectName(object_name)
-        return label
-
-    def create_button(self, parent, text, min_size=(120, 40), font_size=12,
-                     bold=True, tooltip=None, icon_path=None, object_name=None):
-        """Create a standardized button with consistent styling
-
-        Args:
-            parent: Parent widget
-            text: Button text
-            min_size: Minimum size (default: (120, 40))
-            font_size: Font size (default: 12)
-            bold: Whether font should be bold (default: True)
-            tooltip: Tooltip text (optional)
-            icon_path: Path to icon (optional)
-            object_name: Object name for the button (optional)
-
-        Returns:
-            QPushButton: The created button
-        """
-        button = QtWidgets.QPushButton(parent)
-        button.setMinimumSize(QtCore.QSize(*min_size))
-        font = QtGui.QFont()
-        font.setPointSize(font_size)
-        font.setBold(bold)
-        button.setFont(font)
-        button.setText(text)
-
-        if tooltip:
-            button.setToolTip(tooltip)
-        if icon_path:
-            icon = QtGui.QIcon()
-            icon.addPixmap(QtGui.QPixmap(icon_path), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-            button.setIcon(icon)
-        if object_name:
-            button.setObjectName(object_name)
-
-        return button
-
-    def create_line_edit(self, parent, min_size=(200, 40), font_size=12,
-                        bold=False, placeholder=None, object_name=None):
-        """Create a standardized line edit with consistent styling
-
-        Args:
-            parent: Parent widget
-            min_size: Minimum size (default: (200, 40))
-            font_size: Font size (default: 12)
-            bold: Whether font should be bold (default: False)
-            placeholder: Placeholder text (optional)
-            object_name: Object name for the line edit (optional)
-
-        Returns:
-            QLineEdit: The created line edit
-        """
-        line_edit = QtWidgets.QLineEdit(parent)
-        line_edit.setMinimumSize(QtCore.QSize(*min_size))
-        font = QtGui.QFont()
-        font.setPointSize(font_size)
-        font.setBold(bold)
-        line_edit.setFont(font)
-
-        if placeholder:
-            line_edit.setPlaceholderText(placeholder)
-        if object_name:
-            line_edit.setObjectName(object_name)
-
-        return line_edit
-
-    def create_date_edit(self, parent, enabled=True, min_size=(160, 40),
-                        font_size=12, bold=True, display_format="yyyy-MM-dd",
-                        default_date=None, object_name=None):
-        """Create a standardized date edit with consistent styling
-
-        Args:
-            parent: Parent widget
-            enabled: Whether the date edit is enabled (default: True)
-            min_size: Minimum size (default: (160, 40))
-            font_size: Font size (default: 12)
-            bold: Whether font should be bold (default: True)
-            display_format: Date display format (default: "yyyy-MM-dd")
-            default_date: Default date (QDate object, optional)
-            object_name: Object name for the date edit (optional)
-
-        Returns:
-            QDateEdit: The created date edit
-        """
-        date_edit = QtWidgets.QDateEdit(parent)
-        date_edit.setEnabled(enabled)
-        date_edit.setMinimumSize(QtCore.QSize(*min_size))
-        font = QtGui.QFont()
-        font.setPointSize(font_size)
-        font.setBold(bold)
-        date_edit.setFont(font)
-        date_edit.setCalendarPopup(True)
-        date_edit.setDisplayFormat(display_format)
-
-        if default_date:
-            date_edit.setDate(QtCore.QDate(*default_date))
-        if object_name:
-            date_edit.setObjectName(object_name)
-
-        return date_edit
